25c30a081f36b11f508145a3a4445c29cf1036e5
F1ash <kaperang07@gmail.com>
2011-03-11 18:03:00 +0300

    remake for NewMailAttributes receiving;
    Signed-off-by: F1ash <kaperang07@gmail.com>

--- contents/code/mail.py
+++ contents/code/mail.py
@@ -6,8 +6,12 @@ if __name__ == '__main__':
 	account, passw, fileName = sys.argv[1], sys.argv[2], sys.argv[3]
 	#print account, passw, fileName, '  thread'
 	Result = ( checkMail( [account, passw] ) )
-	suff = ['.Result', '.all', '.new', '.msg']
-	for i in xrange(4) :
-		f = open('/dev/shm/' + fileName + suff[i], 'wb')
-		f.write(str(Result[i]))
+	suff = ['.Result', '.all', '.new', '.msg', '.content']
+	for i in xrange(5) :
+		f = open('/dev/shm/' + fileName + suff[i], 'w')
+		if type(Result[i]) is bool or type(Result[i]) is int :
+			res_ = str(Result[i])
+		else :
+			res_ = Result[i]
+		f.write(res_)
 		f.close
