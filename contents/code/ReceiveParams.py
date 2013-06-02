7a6ce1632334a783cf56ed7e208a9b89a62e646b
F1ash <kaperang07@gmail.com>
2013-06-02 22:59:03 +0400

    some fixes for more convenience;

--- contents/code/ReceiveParams.py
+++ contents/code/ReceiveParams.py
@@ -81,7 +81,8 @@ class ReceiveParams(QWidget):
 
 		self.mailboxLineEdit = QLineEdit()
 		self.mailboxLineEdit.setVisible(False)
-		self.mailboxLineEdit.setToolTip(u'Defailt MailBox : Inbox\nFor example, GMail specified mailbox :\n[Gmail]/All\nor\n[Gmail]/Вся почта')
+		self.mailboxLineEdit.setPlaceholderText(self.tr._translate("Enter monitoring Mail Folder"))
+		self.mailboxLineEdit.setToolTip(u'Defailt monitoring Mail Folder : Inbox\nFor example, GMail specified mailbox :\n[Gmail]/All\nor\n[Gmail]/Вся почта')
 		self.HB2Layout.addWidget(self.mailboxLineEdit, 0, 2)
 
 		self.accountCommand = QComboBox()
@@ -149,7 +150,7 @@ class ReceiveParams(QWidget):
 
 		self.serverLineEdit.setText(self.Settings.value('server').toString())
 
-		if self.Settings.value('Enabled', '0').toString() == '1' :
+		if self.Settings.value('Enabled', '1').toString() == '1' :
 			self.enabledBox.setCheckState(Qt.Checked)
 
 		i = self.connectMethodBox.findData(self.Settings.value('connectMethod', ''), flags = Qt.MatchFixedString)
